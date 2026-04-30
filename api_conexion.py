d61fbd6abc66709edd6561a26e45587d9f71f586
MarianaMendozap <mariana.mendozapl@uanl.edu.mx>
2026-04-30 15:50:20 -0600

    Update api_conexion.py

--- api_conexion.py
+++ api_conexion.py
@@ -1,12 +1,29 @@
 import requests
-
+import json
+ 
 def obtener_datos():
-    """Descarga y retorna los datos JSON de la API."""
-    url = "https://www.football-data.org/"
-    respuesta = requests.get(url)
-    datos = respuesta.json()
-    return datos
-
+    url = "https://api.football-data.org/v4/matches"
+   
+    headers = {
+        "X-Auth-Token": "3468641386934e6f9fbc4344d8347104" 
+    }
+    try:
+        respuesta = requests.get(url, headers=headers)
+    
+        if respuesta.status_code == 200:
+            datos = respuesta.json()
+            return datos
+        else:
+            return f"Información no encontrada"
+    except requests.exceptions.RequestException as e:
+        return f"Ocurrió un error de conexión: {e}"
+ 
 if __name__ == "__main__":
     datos = obtener_datos()
-    print(datos)
+    
+    if isinstance(datos, dict):
+        
+        import json
+        print(json.dumps(datos, indent=4))
+    else:
+        print(datos)
