449fa75ce48f00fc01a03d3e5fdd0cc6d3dbc538
MarianaMendozap <mariana.mendozapl@uanl.edu.mx>
2026-05-11 15:47:08 -0600

    Update procesamiento.py
    Código documentado y con comentarios

--- procesamiento.py
+++ procesamiento.py
@@ -1,10 +1,13 @@
 import api_conexion
 def extraer_datos_partidos(datos):
+    """Recorre la lista de partidos, extrae los nombres de los equipos 
+   y con sus marcadores."""
     partidos = datos.get("matches", [])
-    for idx, partido in enumerate(partidos, 1):
+    for idx, partido in enumerate(partidos, 1): # Recorre cada partido y o enumera (1, 2, 3...)
         local = partido.get("homeTeam", {}).get("name", "Local")
         visitante = partido.get("awayTeam", {}).get("name", "Visitante")
-        goles_local = partido.get("score", {}).get("fullTime", {}).get("home", "-")
+
+        goles_local = partido.get("score", {}).get("fullTime", {}).get("home", "-") # Accede a los goles del tiempo completo 
         goles_visitante = partido.get("score", {}).get("fullTime", {}).get("away", "-")
         print(f"{idx}. {local} {goles_local} - {goles_visitante} {visitante}")
  
